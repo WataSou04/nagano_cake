7cbf5602173cf1ebbcd5a9f67cb6142aad3c675b
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-01-31 14:03:40 +0000

    [Add]退会画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
-  devise_for :customers
-  root to: 'homes#top'
-  get 'homes/about' => 'homes#about', as: 'about'
-  get 'customers/information/edit' => 'customers#edit'
-  get 'customers' => 'customers#show'
-  resources :items, only: [:index, :show]
-  resources :orders, only: [:new, :index, :show]
+  
+  scope module: :public do
+    devise_for :customers
+    root to: 'homes#top'
+    get 'homes/about' => 'homes#about', as: 'about'
+    get 'customers' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/check' => 'customers#check'
+    resources :items, only: [:index, :show]
+    resources :orders, only: [:new, :index, :show]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
