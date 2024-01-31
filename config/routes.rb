5fab7815e1fccb720bd63fd9493ab35f9b17ede8
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-01-31 09:33:40 +0000

    [Add]会員編集画面

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,8 @@ Rails.application.routes.draw do
   devise_for :customers
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-  get 'customers' => 'customers#show'
   get 'customers/information/edit' => 'customers#edit'
-  get 'customers/update'
+  get 'customers' => 'customers#show'
   resources :items, only: [:index, :show]
   resources :orders, only: [:new, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
