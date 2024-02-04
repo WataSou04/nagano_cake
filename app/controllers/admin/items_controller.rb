0c6b78379af5321afa644b8a59e07935763813f2
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-02-04 14:36:54 +0000

    [Add]商品編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -21,9 +21,23 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genre = 
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
   end
   
   private
