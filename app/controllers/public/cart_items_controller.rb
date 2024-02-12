9de9ec2f3c1157b8998f9efc184dd362f6c46706
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-02-12 09:41:15 +0000

    [Add]カート内商品

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,6 +1,17 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cartitem = CartItem.new
     @cartitems = CartItem.all
+  end
+  
+  def create
+    @cartitem = CartItem.new(cartitem_params)
+    @cartitem.save
+    redirect_to cart_items_path
+  end
+  
+  private
+  
+  def cartitem_params
+    params.require(:cart_item).permit(:amount, :item_id, :customer_id)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,6 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
-    @items = Item.all
     @genres = Genre.all
   end
 
