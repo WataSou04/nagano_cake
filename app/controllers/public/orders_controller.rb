554a755a1aa284699272a37a0774de8c562e4203
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-02-17 13:58:13 +0000

    [Fix]注文機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,23 +6,35 @@ class Public::OrdersController < ApplicationController
   end
   
   def check
-    @order = Order.new(order_params)
-    if params[:order][:select_address] == "0"
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.name = current_customer.last_name + current_customer.first_name
-    elsif params[:order][:select_address] == "1"
-      @address = Address.find(params[:order][:address_id])
-      @order.postal_code = @address.postal_code
-      @order.address = @address.address
-      @order.name = @address.name
-    elsif params[:order][:select_address] == "2"
-      @order.customer_id = current_customer.id
-    end
+    @cart_items = CartItem.where(customer_id: current_customer.id)
+    @postage = 800
+    @payment_method = params[:order][:payment_method]
     
-    @cart_items = current_customer.cart_items
-    @order_new = Order.new
-    render :check
+    ary = []
+    @cart_items.each do |cart_item|
+      ary << cart_item.item.with_tax_price * cart_item.amount
+    end
+    @cart_items_price = ary.sum
+    @total_price = @postage + @cart_items_price
+    
+    @address_type = params[:order][:address]
+    case @address_type
+    when "member_address"
+      @selected_address = current_customer.postal_code + " " + current_customer.address + " " + current_customer.last_name + current_customer.first_name
+    when "registered_address"
+      unless params[:order][:registered_address] == ""
+        selected = ShippingAddress.find(params[:order][:registered_address_id])
+        @selected_address = selected.postal_code + " " + selected.address + " " + selected.name
+      else
+        render :new
+      end
+    when "new_address"
+      unless params[:order][:postal_code] == "" && params[:order][:address] == "" && params[:order][:name] == ""
+        @selected_address = params[:order][:postal_code] + " " + params[:order][:address] + " " + params[:order][:name]
+      else
+        render :new
+      end
+    end
   end
 
   def index
