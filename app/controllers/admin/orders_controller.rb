2ca7850f52861424bdb5123cf57d75065bc3e3a0
EC2 Default User <ec2-user@ip-172-31-37-30.ap-northeast-1.compute.internal>
2024-02-23 10:13:23 +0000

    [Fix]管理者注文ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,7 +11,7 @@ class Admin::OrdersController < ApplicationController
   
   def update
     @order = Order.find(params[:id])
-    @order.update
-    redirect_to show
+    @order.update(status: params[:order][:status])
+    redirect_to admin_order_path
   end
 end
